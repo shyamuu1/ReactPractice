7b798704fabc317732f22e5cbc396f703b567dc1
shyamuu1 <shyamuu.vasanjee@gmail.com>
2020-09-30 15:20:07 -0400

    removing old project

--- backend/mongo-practice/src/main/java/com/springmongoparactice/mongopractice/MongoPracticeApplication.java
+++ backend/mongo-practice/src/main/java/com/springmongoparactice/mongopractice/MongoPracticeApplication.java
@@ -1,13 +1,15 @@
 package com.springmongoparactice.mongopractice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-//exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class}
-@SpringBootApplication()
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+import org.springframework.context.annotation.Import;
+
+import com.springmongoparactice.mongopractice.config.MongoConfig;
+
+
+@SpringBootApplication
+@Import(MongoConfig.class)
+//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MongoPracticeApplication {
 
 	public static void main(String[] args) {
