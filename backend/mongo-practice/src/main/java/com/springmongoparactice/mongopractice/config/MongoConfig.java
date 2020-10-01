7562d70f7b03cdfa3d3d07a57b7e9f836f281e72
shyamuu1 <shyamuu.vasanjee@gmail.com>
2020-10-01 12:23:48 -0400

    cleaning up unused dependencies

--- backend/mongo-practice/src/main/java/com/springmongoparactice/mongopractice/config/MongoConfig.java
+++ backend/mongo-practice/src/main/java/com/springmongoparactice/mongopractice/config/MongoConfig.java
@@ -1,19 +1,11 @@
 package com.springmongoparactice.mongopractice.config;
 
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.util.UriUtils;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
@@ -22,18 +14,30 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
 	@Value("${spring.data.mongodb.database}")
 	private String databaseName;
-
+	
+	@Value("${spring.data.mongodb.uri}")
+	private String uri;
+	
 	@Override
 	protected String getDatabaseName() {
 		System.out.print(this.databaseName);
 		return this.databaseName;
 	}
+	
+	protected String getUri() {
+		return this.uri;
+	}
+	
+	protected String setConnectionString(String dbName, String uri) {
+		String conn = uri+dbName+"?retryWrites=true&w=majority";
+		return conn;
+	}
 
 	@Override
 	@Bean
 	public MongoClient mongoClient() {
-		MongoClient mongoClient = MongoClients
-				.create("mongodb+srv://admin:admin@cluster0.dhxhk.mongodb.net/ToDoApp?retryWrites=true&w=majority");
+		String connection = setConnectionString(getDatabaseName(), getUri());
+		MongoClient mongoClient = MongoClients.create(connection);
 		return mongoClient;
 	}
 
