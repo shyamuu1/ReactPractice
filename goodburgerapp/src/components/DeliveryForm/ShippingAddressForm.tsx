7285502fa457aea8b8d0ddb3a928f54d7e93566d
shyamuu1 <shyamuu.vasanjee@gmail.com>
2020-11-16 10:05:50 -0800

    tweaking positioning in forms

--- goodburgerapp/src/components/DeliveryForm/ShippingAddressForm.tsx
+++ goodburgerapp/src/components/DeliveryForm/ShippingAddressForm.tsx
@@ -8,17 +8,25 @@ const AddressForm:React.FC = () => {
             <div>
             <h2>Delivery Address</h2>
             <form className="addressForm">
-                <div className="form-control">
+                <div className="form-group">
                     <label>Street Name</label>
-                    <input type="text"/>
+                    <input type="text" className="form-control"/>
                 </div>
-                <div className="form-control">
+                <div className="form-group">
                     <label>City</label>
-                    <input type="text"/>
+                    <input type="text" className="form-control"/>
                 </div>
-                <div className="form-control">
+                <div className="form-group">
                     <label>Postal Code</label>
-                    <input type="Number" />
+                    <div className="state-select">
+                    <input type="Number" className="form-control" maxLength={5} />
+                    <label>State </label>
+                    <select>
+                        <option value="Florida">FL</option>
+                        <option value="Georgia">GA</option>
+                        <option value="New Mexico">NM</option>
+                    </select>
+                    </div>
                 </div>
             </form>
             </div>
