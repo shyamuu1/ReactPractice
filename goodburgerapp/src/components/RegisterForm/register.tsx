148c91882a5862d83f2f7d702103c3b1da397357
shyamuu1 <shyamuu.vasanjee@gmail.com>
2020-11-16 12:01:00 -0800

    adding registration component

---
+++ goodburgerapp/src/components/RegisterForm/register.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./register.css";
+
+const Register:React.FC =() => {
+    return (
+        <div className="register-container">
+            <div className="register-header">
+            </div>
+            <div className="register-body">
+                <form className="register-form">
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" placeholder="Email" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>First Name</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Last Name</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="number" className="form-control"/>
+                    </div>
+                    <div className="register-actions">
+                        <button type="button">Continue to Checkout</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
