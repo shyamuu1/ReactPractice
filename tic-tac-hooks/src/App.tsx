a72b5c92d06fd4818b404ac353e5744049b5c2df
shyam vasanjee <shyamuu.vasanjee@gmail.com>
2021-02-23 17:45:53 -0500

    added state variables and win condition

--- tic-tac-hooks/src/App.tsx
+++ tic-tac-hooks/src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
+import Board from './components/Board/GameBoard';
+import Game from './containers/Game';
+import { calculateWinner } from './GameConditions/winCondition/WinCondition';
 
 const App:React.FC = () => {
+  const gameboard:string[] = Array(9).fill(null);
+  const [history, setHistory] = useState([{squares:gameboard}]);
+  const [isXNext, setX] = useState<boolean>(true);
+  const [turnNumber, setTurn] = useState<number>(0);
+  const current = history[history.length]; 
+  const winner = calculateWinner(current.squares);
+
   return (
     <div className="App">
+      <Game>
+        <Board squares={current.squares} clicked={()=>{}}/>
+      </Game>
       
     </div>
   );
